87130c21c2733983ca4246de8212d3f2a6672c77
ТЕХНОРАЙ <@DESKTOP-E2H7NN2>
2024-10-06 21:21:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿//using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_3._2
+{
+    internal class Program
+
+    {   //Task2: Обчислення суми ряду
+
+        static void SumRyadu ()
+        {
+           double x = -0.5;          
+           double epsilon = 1e-3;  
+           double sum = 0.0;
+           double term;
+           int n = 1;                 
+           double factorial = 0;
+
+
+            do
+            {
+
+                term = Math.Pow(x, n) / factorial;
+                sum += term;
+
+                Console.WriteLine($"Ітерація {n}: Поточний член = {term}, Сума = {sum}");
+
+                n++;
+                factorial++;
+ 
+
+            } while (Math.Abs(term / sum) >= epsilon);
+
+            Console.WriteLine($"Накопичена сума ряду S1 = {sum} після {n} ітерацій");
+
+            Console.WriteLine("\nВиконуємо обчислення суми ряду (Завдання 2):");
+            SumRyadu();
+
+
+
+
+
+
+
+        }
+    }
+}
